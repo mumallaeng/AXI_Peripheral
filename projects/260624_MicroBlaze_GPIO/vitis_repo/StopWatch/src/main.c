d5a9198149df004e75c53c44e51d62d1e891053c
mumallaeng <mumallaeng@outlook.com>
2026-06-24 14:28:05 +0900

    hw: add stopwatch button edge handling

--- projects/260624_MicroBlaze_GPIO/vitis_repo/StopWatch/src/main.c
+++ projects/260624_MicroBlaze_GPIO/vitis_repo/StopWatch/src/main.c
@@ -3,6 +3,49 @@
 #include "driver/FND/FND.h"
 #include "common/delay/delay.h"
 
+typedef enum
+{
+   NO_ACT = 0,
+   ACT_PUSHED,
+   ACT_RELEASED
+} button_status_e;
+
+typedef enum
+{
+   RELEASED = 0,
+   PUSHED
+} button_state_e;
+
+void Button_Init()
+{
+   uint32_t btnPort = GPIO_GetCR(GPIOB);
+   btnPort &= ~(1 << 4 | 1 << 5);
+   GPIO_SetMode(GPIOB, btnPort);
+}
+
+uint32_t Button_GetState()
+{
+   static button_state_e prevState = RELEASED;
+   button_state_e curState = GPIO_ReadPin(GPIOB, GPIO_PIN_4) ? PUSHED : RELEASED;
+
+   if (curState == PUSHED && prevState == RELEASED)
+   {
+      prevState = curState;
+      delay_ms(5);
+      return ACT_PUSHED;
+   }
+   else if (curState == RELEASED && prevState == PUSHED)
+   {
+      prevState = curState;
+      delay_ms(5);
+      return ACT_RELEASED;
+   }
+   else
+   {
+      return NO_ACT;
+   }
+}
+
 int main()
 {
    int counter = 0;
@@ -16,6 +59,7 @@ int main()
    {
       if (Button_GetState() == ACT_RELEASED)
       {
+         LED_PinToggle(0);
       }
       /*********polling service routine***********/
       FND_Excute();
