5acce20c3d986677b6d69d9882b9dde5ed31d062
mumallaeng <mumallaeng@outlook.com>
2026-06-27 17:22:26 +0900

    chore: restore Vitis firmware workspace

---
+++ firmware/AXI_Peripheral_platform/platform.tcl
@@ -0,0 +1,24 @@
+# Re-create the Vitis platform project from the XSA exported by Vivado.
+#
+# Expected XSA:
+#   firmware/AXI_Peripheral_platform/hw/AXI_Peripheral.xsa
+
+set script_dir [file dirname [file normalize [info script]]]
+set workspace_dir [file dirname $script_dir]
+set xsa_path [file join $script_dir hw AXI_Peripheral.xsa]
+
+platform create -name {AXI_Peripheral_platform} \
+    -hw $xsa_path \
+    -out $workspace_dir
+
+platform write
+domain create -name {standalone_microblaze_0} \
+    -display-name {standalone_microblaze_0} \
+    -os {standalone} \
+    -proc {microblaze_0} \
+    -runtime {cpp} \
+    -arch {32-bit} \
+    -support-app {empty_application}
+platform generate -domains
+platform active {AXI_Peripheral_platform}
+platform generate -quick
